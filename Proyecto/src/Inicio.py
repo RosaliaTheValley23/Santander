28d00682e05dee6616c74e37295c308c3a602553
RosaliaTheValley23 <127755032+RosaliaTheValley23@users.noreply.github.com>
2023-03-13 23:26:07 +0100

    Update Inicio.py

--- Proyecto/src/Inicio.py
+++ Proyecto/src/Inicio.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 
 current_dir = os.path.dirname(os.path.realpath(__file__)) 
-path_imagen = os.path.join(current_dir, 'media_stock//Banco_Santander_Logotipo.png')
+path_imagen = os.path.join(current_dir, 'media_stock/Banco_Santander_Logotipo.png')
 
 st.set_page_config(page_title="Santander People Analytics")
 st.image(path_imagen)
