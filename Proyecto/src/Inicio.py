29a4b95df126a582d6dcf4241302227d48b3cbda
RosaliaTheValley23 <127755032+RosaliaTheValley23@users.noreply.github.com>
2023-03-13 23:20:13 +0100

    Update Inicio.py

--- Proyecto/src/Inicio.py
+++ Proyecto/src/Inicio.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 
 
-data = pd.read_excel("../data/Data_streamlit.xlsx")
 st.set_page_config(page_title="Santander People Analytics")
 st.image("../media_stock/Banco_Santander_Logotipo.png")
 st.header('TheGoodWay')
