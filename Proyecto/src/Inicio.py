2bad99bb83028c55e4c5981c6e8e48c6daaa7b4b
RosaliaTheValley23 <127755032+RosaliaTheValley23@users.noreply.github.com>
2023-03-13 16:17:53 +0100

    Update Inicio.py

--- Proyecto/src/Inicio.py
+++ Proyecto/src/Inicio.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-from datetime import date
-import altair as alt
-import matplotlib.pyplot as plt
+
 
 
 data = pd.read_excel("../data/Data_streamlit.xlsx")
@@ -11,7 +9,4 @@ st.image("../media_stock/Banco_Santander_Logotipo.png")
 st.header('TheGoodWay')
 st.markdown('---')
 
-
-# cd C:\Users\u1132046\OneDrive - IQVIA\Desktop\THE VALLEY\Proyecto\src
-# streamlit run Inicio.py
 
