83effa36d0ceeab166c7fd19321534339161afae
RosaliaTheValley23 <127755032+RosaliaTheValley23@users.noreply.github.com>
2023-03-13 23:23:05 +0100

    Update Inicio.py

--- Proyecto/src/Inicio.py
+++ Proyecto/src/Inicio.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import os
 
 current_dir = os.path.dirname(os.path.realpath(__file__)) 
 path_imagen = os.path.join(current_dir, 'media_stock//Banco_Santander_Logotipo.png')
