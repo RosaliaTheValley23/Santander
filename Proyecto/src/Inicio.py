539cc703ad968f72d243115a067b3f244d8121d3
RosaliaTheValley23 <127755032+RosaliaTheValley23@users.noreply.github.com>
2023-03-14 09:44:03 +0100

    Update Inicio.py

--- Proyecto/src/Inicio.py
+++ Proyecto/src/Inicio.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import os
 
-current_dir = os.path.dirname(os.path.realpath(__file__)) 
-path_imagen = os.path.join(current_dir, 'media_stock/Banco_Santander_Logotipo.png')
+# current_dir = os.path.dirname(os.path.realpath(__file__)) 
+# path_imagen = os.path.join(current_dir, 'media_stock/Banco_Santander_Logotipo.png')
 
 st.set_page_config(page_title="Santander People Analytics")
-# st.image(path_imagen)
+# st.image('/app/santander/Proyecto/media_stock/Banco_Santander_Logotipo.png')
 st.header('TheGoodWay')
 st.markdown('---')
